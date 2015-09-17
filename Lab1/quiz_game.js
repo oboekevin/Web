fb336b0394239708a9154918449fd80d9394d572
Kevin Chaplin <kevinchaplin58@gmail.com>
2015-09-17 15:07:34 -0400

    Final refinements on quiz game, including test / not test mode and instructions

--- Lab1/quiz_game.js
+++ Lab1/quiz_game.js
@@ -1,12 +1,28 @@
 var pkmn_list = ['Chespin', 'Quilladin', 'Chesnaught', 'Fennekin', 'Braixen', 'Delphox', 'Froakie', 'Frogadier', 'Greninja', 'Bunnelby', 'Diggersby', 'Fletchling', 'Fletchinder', 'Talonflame', 'Scatterbug', 'Spewpa', 'Vivillon', 'Litleo', 'Pyroar', 'Flabébé', 'Floette', 'Florges', 'Skiddo', 'Gogoat', 'Pancham', 'Pangoro', 'Furfrou', 'Espurr', 'Meowstic', 'Honedge', 'Doublade', 'Aegislash', 'Spritzee', 'Aromatisse', 'Swirlix', 'Slurpuff', 'Inkay', 'Malamar', 'Binacle', 'Barbaracle', 'Skrelp', 'Dragalge', 'Clauncher', 'Clawitzer', 'Helioptile', 'Heliolisk', 'Tyrunt', 'Tyrantrum', 'Amaura', 'Aurorus', 'Sylveon', 'Hawlucha', 'Dedenne', 'Carbink', 'Goomy', 'Sliggoo', 'Goodra', 'Klefki', 'Phantump', 'Trevenant', 'Pumpkaboo', 'Gourgeist', 'Bergmite', 'Avalugg', 'Noibat', 'Noivern', 'Xerneas', 'Yveltal', 'Zygarde', 'Diancie'];
-var ROWS = 1; //14;
+var ROWS = 14;
 var COLS = 5;
 var first_id = 650;
-var last_id = 654; // 719;
+var last_id = 719;
 var num_pokemon = last_id - first_id + 1;
 var number_completed = 0;
-var total_time = 20;
+var total_time = 300;
 var interval_id;
+
+function test_mode() {
+	ROWS = 1;
+	last_id = 654;
+	num_pokemon = last_id - first_id + 1;
+	total_time = 20;
+	var cheat = "First 5 Gen 6 Pokemon: ";
+	for (k = 0; k < num_pokemon; k++) {
+		cheat += pkmn_list[k];
+		if (k < num_pokemon - 1) { 
+			cheat += ", ";
+		}
+	}
+	document.getElementById('cheat_sheet').innerHTML = cheat;
+	make_table();
+}
 
 function input_answer(text) {
 	// alert('hi');
@@ -16,6 +32,7 @@ function input_answer(text) {
 				document.getElementById("pkmn" + (k + first_id)).innerHTML = (k + first_id) + " - " + pkmn_list[k];
 				number_completed++;
 				document.getElementById("input_box").value = "";
+				document.getElementById('progress').innerHTML = number_completed + " / " + num_pokemon;
 				if (number_completed == num_pokemon) {
 					end_game();
 				}
@@ -65,12 +82,14 @@ function new_game() {
 	document.getElementById('quit_button').removeAttribute('disabled');
 	document.getElementById('input_box').removeAttribute('disabled');
 	document.getElementById('input_box').focus();
-	document.getElementById('timing').innerHTML = "Time remaining: <span id=\"timer\">20</span>";
+	document.getElementById('timing').innerHTML = "Time remaining: <span id=\"timer\">" + total_time + "</span>";
 	start_timer();
+	document.getElementById('progress').innerHTML = number_completed + " / " + num_pokemon;
 }
 
 function end_game() {
 	clearInterval(interval_id);
+	document.getElementById('progress').innerHTML = "";
 	message = 'Result: ' + number_completed + " / " + num_pokemon
 	message += " - ";
 	message += (num_pokemon == number_completed) ? "you won!" : "you lost";
