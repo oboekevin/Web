0de7d0984f43e3f193059160ad912f4679d972e0
Kevin Chaplin <kevinchaplin58@gmail.com>
2015-09-24 16:01:04 -0400

    Slight progress on shuffle of questions for quiz game 2

--- Lab2/quiz_game2.js
+++ Lab2/quiz_game2.js
@@ -1,26 +1,74 @@
-q = {picture: "blue_rose.jpg", question: "What color is this?", response:"blue"};
+function Question(pic, que, re) {
+	this.picture = pic;
+	this.question = que;
+	this.response = re;
+}
 
-// function pose_question(q) {
-// 	document.getElementById("picture").src = q.picture;
-// 	document.getElementById("question").innerHTML = q.question;
-// };
+var questions = [new Question("blue_rose.jpg", "What color is this?", "blue"),
+				new Question(null, "What's my favorite color?", "blue")]
+var q = questions[0];
+var q_index = 0;
+var score = 0;
 
 function check_answer(ans) {
-	if (quiz_item.response.toUpperCase() == ans.toUpperCase()) {
-		alert('yup');
-	} else {
-		//
+	if (q.response.toUpperCase() == ans.toUpperCase()) {
+		score++;
+		next_question();
 	}
 };
 
-$(document).ready(function(){
-	// alert('hi');
-	// pose_question(quiz_item);
-	$("#picture").src = q.picture;
-	$("#question").innerHTML = q.question;
-});
+function update_question() {
+	if (q.picture === null) {
+		$("#picture").hide();
+	} else {
+		$("#picture").show();
+		$("#picture").attr("src", q.picture);
+	}
+	$("#question").text(q.question);
+};
 
-// $("#response").change(function() {
-// 	alert('hi');
-// 	check_answer($(this).value);
-// });
+function next_question() {
+	$("#response").val("");
+	q_index = (q_index + 1); // % questions.length;
+	if (q_index < questions.length)
+	{
+		q = questions[q_index];
+		update_question();
+	} else {
+		end_game();
+	}
+};
+
+function end_game() {
+	$("#quiz_zone").hide();
+	$("#results").show();
+	$("#score").text("Score: " + score + " / " + questions.length);
+};
+
+function reset_game() {
+	$("#quiz_zone").show();
+	$("#results").hide();
+	shuffle_questions();
+	q = questions[0];
+	q_index = 0;
+	score = 0;
+}
+
+function shuffle_questions() {
+	// alert('hi??');
+	var q2 = new Array();
+	l = questions.length;
+	for (i = l; i > 0; i--) {
+		k = Math.floor(Math.random() * i)
+		q2.push(questions[k]);
+		questions.splice(k, k + 1);
+	};
+	questions = q2;
+	// alert(questions.length);
+};
+
+$(document).ready(function(){
+	update_question();
+	$("#results").hide();
+	shuffle_questions();
+});
