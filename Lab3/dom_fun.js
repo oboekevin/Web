580429cf33dc34dae57981b7d811f09c9f67fb13
Kevin Chaplin <kevinchaplin58@gmail.com>
2015-10-01 15:12:18 -0400

    Finish DOM funsies

--- Lab3/dom_fun.js
+++ Lab3/dom_fun.js
@@ -1,16 +1,38 @@
 function magic(event) {
-	// alert('hi');
 	var ele = event.target;
-	element_after(ele).style.backgroundColor = "green";
+	var next = element_after(ele);
+	// next.style.opacity = "0.5";
+	fade(next);
+	// element_after(ele).style.backgroundColor = "green";
 };
-function element_after(ele) {
-	if (ele.childElementCount > 0) {
-		// alert('hi')
-		return ele.firstElementChild;
+function element_after(ele, skip) {
+	if (skip == true) {
+		//
+	} else {
+		if (ele.childElementCount > 0) {
+			// alert('hi')
+			return ele.firstElementChild;
+		}
 	}
-	num_siblings = ele.parentElement.childElementCount - 1;
-	if (ele.parentElement.lastElementChild == ele) {
-		return elementa_after(ele.parentElement)
+	if (ele.nextElementSibling != null) {
+		return ele.nextElementSibling;
 	}
-	// if (ele.parentNode)
+	return element_after(ele.parentElement, true);
+};
+function fade(elem) {
+	// alert('howdy')
+	elem.style.opacity = 0;
+	var interval_id = setInterval(function () {
+		// alert(typeof ele.style.opacity);
+		var op = parseFloat(elem.style.opacity)
+		// alert(typeof op)
+		// alert(op)
+		if (op < 1) {
+			// alert(op + 0.1);
+			elem.style.opacity = op + 0.1;
+			// alert(elem.style.opacity)
+		} else {
+			clearInterval(interval_id);
+		}
+	}, 100);
 };
