268fca245159416f7ea7aabeb126f656ef2aee9f
Kevin Chaplin <kevinchaplin58@gmail.com>
2015-10-20 15:29:34 -0400

    Finish Forms Lab

--- Lab4/forms.js
+++ Lab4/forms.js
@@ -1,11 +1,62 @@
-var food_challenges = {"spice": ["spicy food 1", "spicy food 2", "spicy food 3"],
-	"quant": ["massive hamburger 1", "massive hamburger 2", "massive hamburger 3"],
-	"qual": ["haute cuisine 1", "haute cuisine 2", "haute cuisine 3"],
-	"fat": ["deep fried oreos", "fried butter", "bacon french fries"],
-	"??": ["?? 1", "?? 2", "?? 3"]}
+var food_challenges = {"spice": ["Flaming Tongue Tacos", "Devil's Sauce Sub", "Indian Kebabs", "5Guys Cajun Fries"],
+	"quant": ["Colossal Burger", "5 Foot Pizza", "Mile of Noodles"],
+	"qual": ["Escargot", "Creme brulee", "Truffles"],
+	"fat": ["Deep Fried Oreos", "Fried Butter", "Bacon French Fries"],
+	"??": ["Pickled Watermelon Rinds", "Crickets", "Fried Grapes"]}
 
 function main() {
 	// document.getElementByName("username").focus();
+}
+
+function validate_form() {
+	var form = document.forms["f"]
+
+	//----- username -----//
+	var re = /^\w+$/
+	if (! validate_field(form, "username", re)) {
+		return false;
+	}
+
+	//-----city-----//
+	re = /^[A-z]+$/
+	if (! validate_field(form, "city", re)) {
+		return false;
+	}
+
+	//-----state-----//
+	re = /^[A-z]+$/
+	if (! validate_field(form, "state", re)) {
+		return false;
+	}
+
+	//-----zip-----//
+	re = /^\d{5}(-\d{4})?$/
+	if (! validate_field(form, "zip", re)) {
+		return false;
+	}
+
+	//-----date-----//
+	re = /^\d{2}\/\d{2}\/\d{2}$/
+	if (! validate_field(form, "date", re)) {
+		return false;
+	}
+
+	//-----phone-----//
+	re = /^\d{3}\-\d{3}\-\d{4}$/
+	if (! validate_field(form, "phone", re)) {
+		return false;
+	}
+}
+
+function validate_field(form, field_name, regular_exp) {
+	if (regular_exp.test(form[field_name].value)) {
+		form[field_name].style.backgroundColor = "#FFFFFF";
+		return true;
+	} else {
+		form[field_name].style.backgroundColor = "#FFAAAA";
+		form[field_name].focus();
+		return false;
+	}
 }
 
 function challenge_type(select) {
