14c8e09abca3fa1170ceac9aeb4b478738189004
Kevin Chaplin <kevinchaplin58@gmail.com>
2015-10-29 15:46:18 -0400

    add semicolons bc the php didn't work

--- Lab5/scrabble.php
+++ Lab5/scrabble.php
@@ -1,7 +1,7 @@
 <?php
 $letters = $_REQUEST["set"];
-$pattern = "/^[" . $letters . "]+$/m"
-$dict = file_get_contents("dictionnary.txt")
-preg_match($pattern, $dict, $matches)
+$pattern = "/^[" . $letters . "]+$/m";
+$dict = file_get_contents("dictionnary.txt");
+preg_match($pattern, $dict, $matches);
 echo json_encode($matches);
 ?>
