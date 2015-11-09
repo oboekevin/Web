57ab3ddbcc9c6e2eb088698a1ae0bb4b54ff2a6b
Kevin Chaplin <kevinchaplin58@gmail.com>
2015-11-09 12:12:44 -0500

    work on formatting results for scrabble lab

--- Lab5/scrabble.js
+++ Lab5/scrabble.js
@@ -1,3 +1,27 @@
+function display_results(response) {
+	// document.getElementById("result").innerHTML = response;
+	var results = response.split('<br/>');
+	results[0] = "<span id='best'>" + results[0] + "</span>";
+	for (var i = 0; i < results.length; i++) {
+		results[i] = '<span class="box">' + results[i] + '</span>';
+	};
+	var out = results.join('\n');
+	document.getElementById("result").innerHTML = out;
+}
+
+// function start_timer() {
+// 	interval_id = setInterval(timer_helper, 400);
+// };
+
+// function timer_helper() {
+// 	var time = parseInt(document.getElementById('timer').innerHTML);
+// 	if (time == 0) {
+// 		end_game();
+// 	} else {
+// 		document.getElementById('timer').innerHTML = "" + (time - 1)
+// 	}
+// }
+
 function getWords() {
 	var letters = document.getElementById('letters').value;
 	if(letters.length == 0) return;
@@ -5,7 +29,8 @@ function getWords() {
 	xmlhttp.onreadystatechange = function() {
 		document.getElementById("result").innerHTML = 'waiting';
 		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			document.getElementById("result").innerHTML = xmlhttp.responseText;
+			display_results(xmlhttp.responseText)
+			// document.getElementById("result").innerHTML = xmlhttp.responseText;
 		}
 	}
 	var args = {
