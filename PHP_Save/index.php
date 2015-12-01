e4d4217fee59a4a1f2bfd0a4540d1e2e962ba2df
Kevin Chaplin <kevinchaplin58@gmail.com>
2015-12-01 15:27:26 -0500

    added some sass

--- PHP_Save/index.php
+++ PHP_Save/index.php
@@ -3,22 +3,23 @@
 		<title>
 			Keep Count
 		</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
 		<?php 
-// file_exists
-// file_get_contents
-// file_put_contents
-$filename = "visit_count.txt";
-if(file_exists($filename)) {
-	$count = intval(file_get_contents($filename)) + 1;
-} else {
-	$count = 1;
-}
-echo "<span>This page has been visited ";
-echo $count;
-echo " times.</span>";
-file_put_contents($filename, $count);
+			// file_exists
+			// file_get_contents
+			// file_put_contents
+			$filename = "visit_count.txt";
+			if(file_exists($filename)) {
+				$count = intval(file_get_contents($filename)) + 1;
+			} else {
+				$count = 1;
+			}
+			echo "<span>This page has been visited ";
+			echo $count;
+			echo " times.</span>";
+			file_put_contents($filename, $count);
 		?>
 	</body>
 </html>
