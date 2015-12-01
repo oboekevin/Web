37f22b18542b090ca12f9baf9d46c494c75383e5
oboekevin <oboekevin@users.noreply.github.com>
2015-12-01 14:51:12 -0500

    fill in index.php

--- PHP_Save/index.php
+++ PHP_Save/index.php
@@ -1,1 +1,24 @@
-
+<html>
+	<head>
+		<title>
+			Keep Count
+		</title>
+	</head>
+	<body>
+		<?php 
+// file_exists
+// file_get_contents
+//
+$filename = "visit_count.txt";
+if(file_exists($filename)) {
+	$count = intval(file_get_contents($filename)) + 1;
+} else {
+	$count = 1;
+}
+echo "<span>This page has been visited ";
+echo $count;
+echo " times.</span>";
+// file_put_contents($filename, $count);
+		?>
+	</body>
+</html>
