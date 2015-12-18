75fd875dcb923b2d711c94015b8ed71f4e3d6d24
oboekevin <kevinchaplin58@gmail.com>
2015-12-18 11:43:33 -0500

    get dictionary working, still some bugs and ugliness

--- API/api.js
+++ API/api.js
@@ -1,20 +1,48 @@
 function translation() {
 	// https://glosbe.com/gapi/translate?from=eng&dest=fra&format=xml&phrase=cat&pretty=true
 	var word = document.getElementById('word').value;
-	// if(word.length==0) return;
+	var e = document.getElementById("drop");
+	var orig = e.options[e.selectedIndex].value;
+	var destin = (orig == 'eng') ? 'epo' : 'eng';
+	console.log(word);
+	console.log(orig);
+	console.log(destin);
+	if(word.length == 0) {
+		word = 'cat';
+		orig = 'eng';
+		destin = 'epo';
+	}
+
 	var xmlhttp = new XMLHttpRequest({mozSystem: true});
 	xmlhttp.onreadystatechange = function() {
 		document.getElementById("result").innerHTML = 'waiting';
 		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 			document.getElementById("result").innerHTML = xmlhttp.responseText;
 			j = JSON.parse(xmlhttp.responseText);
+			// console.log(xmlhttp.responseText);
 			document.getElementById("result").innerHTML = j["tuc"][0]["phrase"]["text"];
+			table = document.getElementById("examples");
+			table.innerHTML = "";
+			examples = j["examples"];
+			for (var i = 0; i < examples.length; i++) {
+				tr = document.createElement("tr");
+				td1 = document.createElement("td");
+				t1 = document.createTextNode(examples[i]["first"]);
+				td1.appendChild(t1);
+				td2 = document.createElement("td");
+				t2 = document.createTextNode(examples[i]["second"]);
+				td2.appendChild(t2);
+				tr.appendChild(td1);
+				tr.appendChild(td2);
+				table.appendChild(tr);
+				console.log(examples[i]);
+			};
 		}
 	}
 	var args = {
-		from: 'eng',
-		dest: 'epo',
-		phrase: 'cat',
+		from: orig,
+		dest: destin,
+		phrase: word,
 		tm: true,
 		format: 'json',
 		pretty: true
