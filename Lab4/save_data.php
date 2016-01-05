808a7c2a5fa8e1e85ffcc674f6800e3d9b768e84
Kevin Chaplin <kevinchaplin58@gmail.com>
2016-01-05 15:06:01 -0500

    Save Lab4 data to database

--- Lab4/save_data.php
+++ Lab4/save_data.php
@@ -1,3 +1,46 @@
+<html>
+<head>
+	<title>Forms Lab</title>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css" />
+	<link rel="stylesheet" type="text/css" href="../style.css" />
+</head>
+<body>
 <?php
-echo $_REQUEST;
+	// echo json_encode($_REQUEST);
+	echo "<h1>Hello, ";
+	echo $_REQUEST["username"];
+	echo "!</h1>";
+	echo "<h2>So, you live in ";
+	echo $_REQUEST["city"];
+	echo ", ";
+	echo $_REQUEST["state"];
+	echo "?</h2><br />";
+
+	echo "<div hidden>";
+
+	$r = $_REQUEST;
+	$person = [$r["username"], $r["city"], $r["state"], $r["zip"], $r["email"], $r["date"], $r["phone"], ];
+
+	$db = new SQLite3('people.db');
+
+	echo sql_statement($person);
+	echo " ~~~ ";
+	$success = $db->exec(sql_statement($person));
+	echo json_encode($success);
+	echo "<br/>";
+
+	echo "</div>";
+
+	function sql_statement($person) {
+		$base = "INSERT INTO People VALUES('";
+		$end = "');";
+		return $base . implode("', '", $person) . $end;
+	}
 ?>
+</body>
+</html>
+
+<!-- {"username":"kevin","city":"Alexandria","state":"VA",
+"zip":"22310","email":"kevinchaplin58@gmail.com",
+"date":"05\/01\/16","phone":"703-201-4938",
+"pick_drop":"a","quant0":"on","quant1":"on"} -->
